48abef6275bc7e75fbc9bae9dc35715d7a71bb53
y4hm1 <146053942+y4hm1@users.noreply.github.com>
2023-10-09 09:54:45 +0530

    Add files via upload

---
+++ d8e1/script.js
@@ -0,0 +1,38 @@
+
+
+let box1 = document.getElementById("box1");
+let box2 = document.getElementById("box2");
+let box3 = document.getElementById("box3");
+let box4 = document.getElementById("box4");
+
+function myFunction1() {
+    document.getElementById("one").src="./images/sukuna2.jpeg";
+    box1.style.color="red";
+    box1.style.backgroundColor="black";
+
+}
+function myFunction2() {
+    document.getElementById("two").src="./images/hei2.png";
+    box2.style.color="blue";
+    box2.style.backgroundColor="purple";
+    box2.style.fontSize="60px";
+    box2.style.display="inline";
+
+
+}
+function myFunction3() {
+    document.getElementById("three").src="./images/zora2.png";
+    box3.style.color="yellow";
+    box3.style.backgroundColor="white";
+    box3.style.fontSize="40px";
+    box3.style.display="inline";
+
+}
+function myFunction4() {
+    document.getElementById("four").src="./images/zenitsu2.png";
+    box4.style.color="white";
+    box4.style.backgroundColor="yellow";
+    box4.style.fontSize="50px";
+    box4.style.height="1000px";
+    box4.style.width="1000px";
+}
