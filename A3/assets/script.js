654ce27c54be8dcde31acb1e1eac6d92093c19a7
y4hm1 <146053942+y4hm1@users.noreply.github.com>
2023-10-26 14:29:20 +0530

    Add files via upload

---
+++ A3/assets/script.js
@@ -0,0 +1,5 @@
+// JAVASCRIPT COMES HERE
+// --------------------------------------------------------
+gsap.registerPlugin(TextPlugin);
+
+gsap.to("#title", {duration: 2, text: "The Time Traveler's Dilemma", delay: 2});
